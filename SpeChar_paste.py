d150bf3e764424213c6e99aa0c7c844f24e516eb
tubbadu <ale.ribau@gmail.com>
2021-05-25 22:59:37 +0200

    added little delay before pasting

--- SpeChar_paste.py
+++ SpeChar_paste.py
@@ -1,9 +1,11 @@
 from pyautogui import hotkey
 import sys
+from time import sleep
 try:
 	flag = sys.argv[1]
 except:
 	flag = "noflag"
+sleep(0.1)
 if flag == 'shift':
 	hotkey('ctrl', 'shift', 'v')
 elif flag == 'noflag' or flag == '':
